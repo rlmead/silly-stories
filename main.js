7fdd59fe69b6ba7a0178ff9a1e084fa39f6d7f95
Rebecca Mead <beckyleemead@gmail.com>
2020-09-09 13:17:08 -0400

    format main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
   return array[random];
 }
 
@@ -21,22 +21,22 @@ function result() {
 
   let newStory = storyText;
 
-  if(customName.value !== '') {
+  if (customName.value !== '') {
     let name = customName.value;
-    newStory = newStory.replace('Bob',name)
+    newStory = newStory.replace('Bob', name)
   }
 
-  if(document.getElementById("uk").checked) {
-    let weight = Math.round(300/14)+' stone';
-    let temperature =  Math.round((94-32)*5/9)+' celsius';
-    newStory = newStory.replace('300 pounds',weight);
-    newStory = newStory.replace('94 fahrenheit',temperature);
+  if (document.getElementById("uk").checked) {
+    let weight = Math.round(300 / 14) + ' stone';
+    let temperature = Math.round((94 - 32) * 5 / 9) + ' celsius';
+    newStory = newStory.replace('300 pounds', weight);
+    newStory = newStory.replace('94 fahrenheit', temperature);
   }
-  
-  newStory = newStory.replace(':insertx:',randomValueFromArray(insertX));
-  newStory = newStory.replace(':insertx:',randomValueFromArray(insertX));
-  newStory = newStory.replace(':inserty:',randomValueFromArray(insertY));
-  newStory = newStory.replace(':insertz:',randomValueFromArray(insertZ));
+
+  newStory = newStory.replace(':insertx:', randomValueFromArray(insertX));
+  newStory = newStory.replace(':insertx:', randomValueFromArray(insertX));
+  newStory = newStory.replace(':inserty:', randomValueFromArray(insertY));
+  newStory = newStory.replace(':insertz:', randomValueFromArray(insertZ));
   story.textContent = newStory
   story.style.visibility = 'visible';
 }
